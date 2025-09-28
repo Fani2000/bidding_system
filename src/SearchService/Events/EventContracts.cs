f7f2a99424e921798d0471a66e71697b8178d94e
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2025-09-28 13:16:35 +0200

    Updated the models for our searchDB

--- src/SearchService/Events/EventContracts.cs
+++ src/SearchService/Events/EventContracts.cs
@@ -1,29 +1,8 @@
 using System;
 
-namespace BiddingService.Events
+namespace SearchService.Contracts
 {
-    // Emitted when a bid is placed
-    public class BidPlaced
-    {
-        public string Id { get; set; } = string.Empty;
-        public string AuctionId { get; set; } = string.Empty;
-        public string Bidder { get; set; } = string.Empty;
-        public DateTime BidTime { get; set; }
-        public int Amount { get; set; }
-        public string BidStatus { get; set; } = string.Empty;
-    }
-
-    // Emitted when an auction finishes
-    public class AuctionFinished
-    {
-        public bool ItemSold { get; set; }
-        public string AuctionId { get; set; } = string.Empty;
-        public string Winner { get; set; } = string.Empty;
-        public string Seller { get; set; } = string.Empty;
-        public int Amount { get; set; }
-    }
-
-    // Consumed when an auction is created in Auction Service
+    // Raised when a new auction is created in the Auction Service
     public class AuctionCreated
     {
         public Guid Id { get; set; }
@@ -31,7 +10,7 @@ namespace BiddingService.Events
         public DateTime UpdatedAt { get; set; }
         public DateTime AuctionEnd { get; set; }
         public string Seller { get; set; } = string.Empty;
-        public string Winner { get; set; } = string.Empty;
+        public string? Winner { get; set; }
         public string Make { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
         public int Year { get; set; }
@@ -42,5 +21,42 @@ namespace BiddingService.Events
         public int ReservePrice { get; set; }
         public int? SoldAmount { get; set; }
         public int? CurrentHighBid { get; set; }
+    }
+
+    // Raised when an auction is updated
+    public class AuctionUpdated
+    {
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int Mileage { get; set; }
+        public int Year { get; set; }
+    }
+
+    // Raised when an auction is deleted
+    public class AuctionDeleted
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+
+    // Raised when an auction is finished
+    public class AuctionFinished
+    {
+        public bool ItemSold { get; set; }
+        public string AuctionId { get; set; } = string.Empty;
+        public string Winner { get; set; } = string.Empty;
+        public string Seller { get; set; } = string.Empty;
+        public int? Amount { get; set; }
+    }
+
+    // Raised when a bid is placed
+    public class BidPlaced
+    {
+        public string Id { get; set; } = string.Empty;
+        public string AuctionId { get; set; } = string.Empty;
+        public string Bidder { get; set; } = string.Empty;
+        public DateTime BidTime { get; set; }
+        public int Amount { get; set; }
+        public string BidStatus { get; set; } = string.Empty;
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,15 @@
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,6 +19,22 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+await DB.InitAsync(
+    "SearchDb",
+    MongoClientSettings.FromConnectionString(
+        builder.Configuration.GetConnectionString("MongoDbConnection")
+            ?? throw new InvalidOperationException("Connection string 'MongoDb' not found.")
+    )
+);
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
+
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
