ec04a3329b569b9b4ad38bccfedf4f0ee21d6cf2
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2025-10-12 05:03:43 +0200

    Added the k8s resouces for our apps

---
+++ push-all.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+
+USERNAME="keorapetse159"
+SERVICES=("auction-svc" "search-svc" "identity-svc" "gateway-svc" "bid-svc" "notify-svc" "web-app")
+
+docker compose build
+
+for SERVICE in "${SERVICES[@]}"; do
+  docker tag ${SERVICE}:latest ${USERNAME}/${SERVICE}:latest
+  docker push ${USERNAME}/${SERVICE}:latest
+done
